d585bd00b2709fde69f8ebcdcefc4dc60676e5b1
Warren D. Rezane <warzkie123@gmail.com>
2018-12-19 16:53:40 +0800

    Rooms images nalang jud kulang jake, okay nata

--- datafile/asd_hotel.sql
+++ datafile/asd_hotel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2018 at 02:17 PM
+-- Generation Time: Dec 18, 2018 at 05:29 AM
 -- Server version: 5.7.15-log
 -- PHP Version: 7.2.5
 
@@ -44,7 +44,8 @@ CREATE TABLE `guests` (
 INSERT INTO `guests` (`guest_id`, `firstname`, `lastname`, `contactno`, `username`, `password`) VALUES
 (1, 'Warren', 'Rezane', '09667591163', 'warshock10', '9538ab335d530f2ebde37a4b32dc396ee5eee758'),
 (2, 'Jake', 'Lendio', '09078215066', 'jake123', 'ce94a14ea2e8a72a58aa4e6c35d7f91c7012bba1'),
-(3, 'Jaime', 'Pinili', '09078215066', 'jaime', 'bf54f59ba326926334d94b932c4940edd1efbf7b');
+(3, 'Jaime', 'Pinili', '09078215066', 'jaime', 'bf54f59ba326926334d94b932c4940edd1efbf7b'),
+(4, 'George', 'Caturza', '09667591163', 'georgedgreat', '80e470018e292bccd77f622caa0586f4bc814aa0');
 
 -- --------------------------------------------------------
 
@@ -64,20 +65,14 @@ CREATE TABLE `histories` (
 --
 
 INSERT INTO `histories` (`history_id`, `guest_id`, `action`, `date_of_action`) VALUES
-(5, 1, 'Warren Rezane reserved Room 11', '2018-11-26 14:54:57'),
-(6, 1, 'Warren Rezane updated his reservation on Room 11', '2018-11-26 14:55:28'),
-(7, 1, 'Warren Rezane cancelled his reservation on Room 11', '2018-11-26 15:18:10'),
-(8, 3, 'Jaime Pinili reserved Room 1', '2018-11-26 15:21:57'),
-(9, 3, 'Jaime Pinili cancelled his reservation on Room 1', '2018-11-26 15:23:30'),
-(10, 3, 'Jaime Pinili reserved Room 3', '2018-11-26 15:24:25'),
-(11, 3, 'Jaime Pinili updated his reservation on Room 3', '2018-11-26 15:32:16'),
-(12, 3, 'Jaime Pinili updated his reservation on Room 3', '2018-11-26 15:32:56'),
-(13, 3, 'Jaime Pinili reserved Room 11', '2018-11-28 10:21:49'),
-(14, 3, 'Jaime Pinili updated his reservation on Room 3', '2018-11-28 11:17:47'),
-(15, 3, 'Jaime Pinili updated his reservation on Room 3', '2018-11-28 11:18:10'),
-(16, 1, 'Warren Rezane reserved Room 2', '2018-11-28 11:20:10'),
-(17, 1, 'Warren Rezane updated his reservation on Room 2', '2018-11-28 11:22:57'),
-(18, 3, 'Jaime Pinili cancelled his reservation on Room 3', '2018-11-28 11:33:41');
+(22, 3, 'Jaime Pinili reserved Room 1', '2018-11-28 17:07:48'),
+(23, 3, 'Jaime Pinili occupied Room 1', '2018-11-28 17:11:54'),
+(24, 3, 'Jaime Pinili has checked out Room 1', '2018-11-28 17:25:44'),
+(25, 1, 'Warren Rezane reserved Room 1', '2018-12-03 12:14:46'),
+(26, 1, 'Warren Rezane cancelled his reservation on Room 1', '2018-12-03 12:14:54'),
+(27, 1, 'Warren Rezane reserved Room 1', '2018-12-03 12:23:56'),
+(28, 1, 'Warren Rezane occupied Room 1', '2018-12-03 12:39:41'),
+(29, 1, 'Warren Rezane has checked out Room 1', '2018-12-03 12:39:54');
 
 -- --------------------------------------------------------
 
@@ -95,16 +90,10 @@ CREATE TABLE `reservations` (
   `check_in` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `check_out` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `roomno` int(11) NOT NULL,
-  `paymenttype` varchar(191) NOT NULL
+  `paymenttype` varchar(191) NOT NULL,
+  `date_reserved` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `days_duration` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `reservations`
---
-
-INSERT INTO `reservations` (`guest_id`, `lastname`, `firstname`, `contactno`, `noofadults`, `noofkids`, `check_in`, `check_out`, `roomno`, `paymenttype`) VALUES
-(3, 'Pinili', 'Jaime', '09078215066', 3, 2, '2018-12-05 16:00:00', '2018-12-07 16:00:00', 11, 'onsite'),
-(1, 'Rezane', 'Warren', '09667591163', 2, 2, '2018-11-29 16:00:00', '2018-12-02 16:00:00', 2, 'onsite');
 
 -- --------------------------------------------------------
 
@@ -127,10 +116,10 @@ CREATE TABLE `rooms` (
 
 INSERT INTO `rooms` (`room_id`, `roomNo`, `roomType`, `rate`, `status`, `guest_id`) VALUES
 (1, 1, 'Standard', 2000, 'Available', 0),
-(2, 2, 'Standard', 2300, 'Reserved', 1),
+(2, 2, 'Standard', 2300, 'Available', 0),
 (3, 3, 'Standard', 2600, 'Available', 0),
 (4, 10, 'Deluxe', 10000, 'Available', 0),
-(5, 11, 'Deluxe', 12000, 'Reserved', 3);
+(5, 11, 'Deluxe', 12000, 'Available', 0);
 
 -- --------------------------------------------------------
 
@@ -193,13 +182,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `guests`
 --
 ALTER TABLE `guests`
-  MODIFY `guest_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `guest_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `histories`
 --
 ALTER TABLE `histories`
-  MODIFY `history_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `history_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `rooms`
